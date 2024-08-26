993a0c7e3ba67ca4637722d28f7db1bb06cbbe3d
root <root@DESKTOP-6C9TRS3>
2024-08-25 22:37:33 -0300

    [fix bug] devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-config.scoped_views = true
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -9,6 +8,7 @@ config.scoped_views = true
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.scoped_views = true
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
