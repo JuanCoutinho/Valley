61f728526f4c69d9f01eb907c2e97963101aae36
JuanCoutinho <juan.devx@gmail.com>
2024-09-26 12:42:56 -0300

    [new feature]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   def perfil
     @user = current_user
   end
+  
+  def after_sign_in_path_for(resource)
+    projects_path # ou a rota desejada
+  end
 
   protected
 
